db947966518d9eb20ee02e1c78bd149bf8047c46
rubickanov <rubickanov@gmail.com>
2025-04-07 18:32:54 +0400

    wdf

--- Source/Game/Weapon.h
+++ Source/Game/Weapon.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#if defined(WIN32) || defined(_WINDOWS)
 #ifdef _EXPORTING
 #define GAME_API __declspec(dllexport)
 #elif _IMPORTING
@@ -7,15 +8,17 @@
 #else
 #define GAME_API
 #endif
+#else
+#define GAME_API __attribute__((visibility("default")))
+#endif
 
 class GAME_API Weapon
 {
-
 public:
-	Weapon(int bullets);
-	bool fire();
-	int damage() const;
+    Weapon(int bullets);
+    bool fire();
+    int damage() const;
 
 private:
-	int m_bullets{23};
+    int m_bullets{23};
 };
